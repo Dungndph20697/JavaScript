38c5e1079a0532ad5fcd8fe9b3dd2c7e4f5d4818
Dungndph20697 <dungthoai321@gmail.com>
2025-06-05 08:45:19 +0700

    thực hành hiển thị đầu ra

---
+++ thHienThi/index.js
@@ -0,0 +1,13 @@
+function showMessage() {
+  alert("Xin chào");
+}
+function showConsole() {
+  console.log("Xin chào");
+}
+function showH2() {
+  document.getElementById("message").innerHTML = "Xin chào";
+}
+
+function showWrite() {
+  document.write("Xin chào");
+}
